fc9c6e663d99651fa5cc9e9cba8142172880467a
Alishba Qureshi <alishbaqureshi593@gmail.com>
2025-09-10 15:10:50 +0500

    Updated header with mobile hamburger menu and responsive CSS

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Education from "./components/education"; // ⬅️ NEW
+import Education from "./components/Education"; // ⬅️ NEW
 import About from "./components/About";
 
 function App() {
